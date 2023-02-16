59aff80e519254881ddd0d0208909a5670d60220
Roy Sher <roysherst@gmail.com>
2023-02-16 18:11:25 +0800

    update bill's swagger operaion

--- bill-service/src/main/java/com/menstalk/billservice/controller/BillDetailController.java
+++ bill-service/src/main/java/com/menstalk/billservice/controller/BillDetailController.java
@@ -13,18 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.menstalk.billservice.domain.BillDetail;
 import com.menstalk.billservice.service.BillDetailService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/bill_detail")
 @RequiredArgsConstructor
 @CrossOrigin
+@Api(tags = "BillDetail Api")
 public class BillDetailController {
 	
 	private final BillDetailService billDetailService;
 	
 	@GetMapping("/{billId}")
 	@ResponseStatus(HttpStatus.OK)
+	@ApiOperation("Show BillDetail by BillId")
 	public List<BillDetail> selectById(@PathVariable Long billId) {
 		
 		return billDetailService.selectByBillId(billId);
