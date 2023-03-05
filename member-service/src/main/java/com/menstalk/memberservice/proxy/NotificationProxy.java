cf62322c0e4a2ad36f8c02198b751f2610196d78
Roy Sher <roysherst@gmail.com>
2023-03-05 16:35:08 +0800

    smaill fix

--- member-service/src/main/java/com/menstalk/memberservice/proxy/NotificationProxy.java
+++ member-service/src/main/java/com/menstalk/memberservice/proxy/NotificationProxy.java
@@ -1,8 +1,6 @@
 package com.menstalk.memberservice.proxy;
 
-import com.menstalk.billservice.dto.NewBillRequest;
 import com.menstalk.memberservice.dto.NewMemberRequest;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
